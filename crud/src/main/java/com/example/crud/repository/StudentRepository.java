c697f6278ec70e5ca64733b529fe3bb99588bb08
Agustin Sileoni <agustinsileoni98@gmail.com>
2024-01-14 13:12:28 -0300

    Agregada una capa de seguridad para los endpoints

--- crud/src/main/java/com/example/crud/repository/StudentRepository.java
+++ crud/src/main/java/com/example/crud/repository/StudentRepository.java
@@ -2,7 +2,7 @@ package com.example.crud.repository;
 
 import org.springframework.stereotype.Repository;
 import org.springframework.data.jpa.repository.JpaRepository;
-import com.example.crud.entity.Student;;;
+import com.example.crud.entity.Student;
 
 
 @Repository
